a05e387511c32da48540e444449cd6ce4d9dc8ba
calvinsongele <48557897+calvinsongele@users.noreply.github.com>
2025-06-06 11:57:33 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+
+// define a default path constant
+define('PATH', getenv('DOCUMENT_ROOT') . '/libs/');
+define('ROOT', getenv('DOCUMENT_ROOT') . '/'); 
+ $rui = explode('/',$_SERVER['REQUEST_URI'] )[1] ?? 'dashboard';
+ $rui = explode('?',$rui)[0];      
+        
+//define('DASHBOARD', getenv('DOCUMENT_ROOT') . '/views/dashboard/');
+define('ADMIN', getenv('DOCUMENT_ROOT') . "/views/{$rui}/");
+define('PROFILE_NAV', "{$rui}" );
+
+define('SERVER', "localhost");
+define('USERNAME', "elightma_calvins");
+define('PASSWORD',"calvrgm22?");
+define('DBNAME', "elightma_healthnavigator");
+define('CODE_VERSION', '1.0.0.0');
+define('SYSTEM', "public/assets/system");
+define('UPLOADS', "public/assets/uploads");
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php 
+require 'config.php';
+require 'util/Auth.php';
+$is_under_maintenace = 0;
+$approved_ips = [];
+  
+    if ( strpos($_SERVER['REQUEST_URI'], '.php') !== false )  {
+        die("
+            <!DOCTYPE html> <html> <head> <title>404 -  Not Found</title> <style>html, body { height: 100%; } </style> </head>
+                <body style='background:black'> <iframe src='views/err/index.php' style='height:100vh;height:100%!important;width:100%;'></iframe> </body> </html>");
+    }
+
+    $url = "http://";  
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') $url = "https://";   
+    
+          
+    // Append the host(domain name, ip) to the URL.   
+    $url.= $_SERVER['HTTP_HOST'];   
+    
+    // Append the requested resource location to the URL   
+    $url.= $_SERVER['REQUEST_URI'];
+    
+    $no_security = explode(':', $url)[0];
+    $valid_url = str_replace('www.', '', $_SERVER["HTTP_HOST"]);
+    
+    if (strpos($url, 'www') == false) {
+        header("Location: https://www." . $valid_url . $_SERVER["REQUEST_URI"], true, 301);
+    } else if ($no_security == 'http') {
+        header("Location: http://www." . $valid_url . $_SERVER["REQUEST_URI"], true, 301);
+    } 
+    
+    
+    if ($is_under_maintenace && (!in_array( $_SERVER['REMOTE_ADDR'], $approved_ips)) ) {
+         
+        die("
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>{$_SERVER['HTTP_HOST']}</title>
+            </head>
+            <body style='background:black'>
+                <section style='margin-top:16%;'>
+                    <h1 style='color:white;'>{$_SERVER['HTTP_HOST']} is under Maintenance</h>
+                </section>
+            </body>
+        </html>");
+    
+    }
+  
+
+spl_autoload_register("autoload");
+
+function autoload($class_name) {
+	$class_path = PATH . $class_name .".php";
+	if (file_exists($class_path))
+		require PATH . $class_name .".php";
+	else {
+		//require_once 'public/pdf/dompdf/autoload.inc.php';
+	}
+}
+
+$app = new App();
+$app->init();
