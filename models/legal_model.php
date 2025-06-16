0954bcb4fed4aede4c6f1eeb2b616c64a1141882
calvinsongele <48557897+calvinsongele@users.noreply.github.com>
2025-06-16 23:16:34 +0300

    Create legal_model.php

---
+++ models/legal_model.php
@@ -0,0 +1,18 @@
+<?php
+ 
+
+class Legal_Model extends Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+ 
+
+
+ 
+
+
+
+    //////////////////////////////////////////////////
+
+}
