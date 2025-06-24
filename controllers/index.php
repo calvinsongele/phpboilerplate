cda54061cc4d78e46cf57710d4ca16398ef0f89a
calvinsongele <48557897+calvinsongele@users.noreply.github.com>
2025-06-24 14:04:48 +0300

    Update index.php

--- controllers/index.php
+++ controllers/index.php
@@ -10,13 +10,13 @@ class Index extends Controller {
 
     public function index() {    
           $this->view->latest = $this->model->getblog();    
-		  $this->view->title = $this->_company()['c_name'] .' | '. $this->_company()['c_short_desc'];
+		  $this->view->title = $this->_company()['c_name'] ;
 		  $this->view->render('index/index' );
     }
     public function search() {
         
            $this->view->search_results = $this->model->search_values(  );
-		  $this->view->title = 'Search | '. $this->_company()['c_short_desc'];
+		  $this->view->title = 'Search' ;
 		  $this->view->render('index/search');
     }
      
